20aff4aafa5d83af7bb2ad802b2b8fdb3b8daee9
DerekNguyen <datnht2602@gmail.com>
2025-09-24 07:45:12 +0700

    [Leetcode-2] Reverse vowels of a string

--- LeetCodeTests/ReverseVowelsOfAString/ReverseVowelsOfAStringTests.cs
+++ LeetCodeTests/ReverseVowelsOfAString/ReverseVowelsOfAStringTests.cs
@@ -3,15 +3,32 @@ namespace LeetCodeTests.ReverseVowelsOfAString;
 public class ReverseVowelsOfAStringTests
 {
     [Theory]
-    [InlineData("IceCream")]
-    public void ReverseVowels(string s) {
+    [InlineData("IceCreAm", "AceCreIm")]
+    [InlineData("leetcode", "leotcede")]
+    public void ReverseVowels(string s, string expectedResult)
+    {
         List<char> vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
         int left = 0;
         int right = s.Length - 1;
-        foreach (var c in s)
+        var listChars = s.ToCharArray();
+        while (left < right)
         {
+            if (!vowels.Contains(listChars[left]))
+            {
+                left++;
+                continue;
+            }
+            if (!vowels.Contains(listChars[right]))
+            {
+                right--;
+                continue;
+            }
+            char temp = listChars[left];
+            listChars[left] = listChars[right];
+            listChars[right] = temp;
+            left++;
+            right--;
         }
-        
-        //Assert.Equal("AceCreIm" ,result);
+        Assert.Equal(expectedResult, new string(listChars));
     }
 }
